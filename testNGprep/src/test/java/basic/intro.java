c8e9b14f411723c2b5bd1495b803bb06714f2203
bkhemanth17 <bkhemanth17@gmail.com>
2026-09-20 02:07:06 +0530

    order of execution and test annotation

--- testNGprep/src/test/java/basic/intro.java
+++ testNGprep/src/test/java/basic/intro.java
@@ -5,16 +5,23 @@ import org.testng.annotations.Test;
 public class intro {
 
     //1st step
+    //it will not follow sequence executes all tes cases based on alphabetical order
+    //use priority() - (priority = 1) - follows priority order
+    // once priority is provided order is not considered we can write where ever we want as methods
+    //priority number can be anything - which ever comes first will be executed
+    //default priority - 0 not provided for @Test
+    //priority is same then execute methods in alphabetical order
+    //negative number are allowed (it has high priority than zero and +ve numbers)
 
-    @Test
+    @Test(priority = 1)
     void openApp(){
         System.out.println("application opened");
     }
-    @Test
+    @Test(priority = 2)
     void login(){
         System.out.println("logged in");
     }
-    @Test
+    @Test(priority = 3)
     void logout(){
         System.out.println("logged out");
     }
